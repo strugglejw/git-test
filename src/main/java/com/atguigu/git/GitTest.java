fadf5c1981ef366ed8df74c1505c35095f7f96ec
jw <3034619731@qq.com>
2022-07-03 18:14:21 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,6 +8,6 @@ public class GitTest {
         System.out.println("hello GitHub!");
 
         System.out.println("hello Git3");
-        System.out.println("hello Git4");
+        System.out.println("hot-fix test!");
     }
 }
